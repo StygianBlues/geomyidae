b3b6d54e4122244a89b50987b122a135b6b40586
Christoph Lohmann <20h@r-36.net>
2012-04-29 08:24:01 +0200

    Optimizing the rc.d.

--- rc.d/Archlinux.rc.d
+++ rc.d/Archlinux.rc.d
@@ -4,33 +4,34 @@
 . /etc/rc.d/functions
 . /etc/conf.d/geomyidae
 
-PID=`pidof -o %PPID /usr/bin/geomyidae`
+PID=$(pidof -o %PPID /usr/bin/geomyidae)
 case "$1" in
-  start)
-    stat_busy "Starting geomyidae"
-    [ -z "$PID" ] && /usr/bin/geomyidae $GEOMYIDAE_ARGS 2>&1
-    if [ $? -gt 0 ]; then
-      stat_fail
-    else
-      add_daemon geomyidae
-      stat_done
-    fi
-    ;;
-  stop)
-    stat_busy "Stopping geomyidae"
-    [ ! -z "$PID" ]  && kill $PID &>/dev/null
-    if [ $? -gt 0 ]; then
-      stat_fail
-    else
-      rm_daemon geomyidae 
-      stat_done
-    fi
-    ;;
-  restart)
-    $0 stop
-    $0 start
-    ;;
-  *)
-    echo "usage: $0 {start|stop|restart}"  
+	start)
+		stat_busy "Starting geomyidae"
+		[ -z "$PID" ] && /usr/bin/geomyidae $GEOMYIDAE_ARGS 2>&1
+		if [ $? -gt 0 ]; then
+			stat_fail
+		else
+			add_daemon geomyidae
+			stat_done
+		fi
+		;;
+	stop)
+		stat_busy "Stopping geomyidae"
+		[ -n "$PID" ] && kill $PID &>/dev/null
+		if [ $? -gt 0 ]; then
+			stat_fail
+		else
+			rm_daemon geomyidae
+			stat_done
+		fi
+		;;
+	restart)
+		$0 stop
+		$0 start
+		;;
+	*)
+		echo "usage: $0 {start|stop|restart}"
 esac
 exit 0
+
