feb461b2232b9b250fa0baebb816f75afa15592e
Christoph Lohmann <20h@r-36.net>
2015-06-21 10:08:18 +0200

    Add OpenBSD rc.d file.
    Thansk Joerg Jung <mail@umaxx.net> !

---
+++ rc.d/OpenBSD.rc.d
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# $OpenBSD: rc.template,v 1.6 2014/08/14 19:54:10 ajacoutot Exp $
+
+daemon="${TRUEPREFIX}/bin/geomyidae"
+daemon_flags="-l /var/log/geomyidae.log -u _geomyidae -g _geomyidae"
+
+. /etc/rc.d/rc.subr
+
+rc_cmd $1
