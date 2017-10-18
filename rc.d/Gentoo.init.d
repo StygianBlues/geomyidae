c34197096e645ab4e8e97fb10027562f90a21bc1
Christoph Lohmann <20h@r-36.net>
2017-10-18 18:08:52 +0200

    Fixing the gentoo init.d script.
    Thank you Sergey Popov for providing the changes.

--- rc.d/Gentoo.init.d
+++ rc.d/Gentoo.init.d
@@ -6,12 +6,12 @@
 start(){
     ebegin "Starting geomyidae"
     [ -n "$GEOMYIDAE_ARGS" ] && GEOMYIDAE_ARGS="-- $GEOMYIDAE_ARGS"
-    start-stop-daemon -Sb -p /var/run/geomyidae.pid -x /usr/sbin/geomyidae $GEOMYIDAE_ARGS
+    start-stop-daemon -Sbm -p /run/geomyidae.pid -x /usr/sbin/geomyidae $GEOMYIDAE_ARGS
     eend $? "Failed to start geomyidae"
 }
 
 stop(){
     ebegin "Stopping geomyidae"
-    start-stop-daemon -S -p /var/run/geomyidae.pid
+    start-stop-daemon -K -p /var/run/geomyidae.pid
     eend $? "Failed to stop geomyidae"
 }
