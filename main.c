StygianBlues/geomyidae

commit 60e5e4b10104014295dd9c9867900f4c72a8cffe

show error/warning on fork failure
this can be useful if there are process resource constraints set also.

Signed-off-by: Christoph Lohmann <20h@r-36.net>

main.c
@@ -401,8 +401,15 @@ main(int argc, char *argv[])
 		}
 	}
 
-	if(dofork && fork() != 0)
-		return 0;
+	if(dofork) {
+		switch(fork()) {
+		case -1:
+			perror("fork");
+			return 1;
+		default:
+			return 0;
+		}
+	}
 
 	if(logfile != nil) {
 		glfd = initlogging(logfile);

handlr.c
@@ -169,6 +169,7 @@ handlecgi(int sock, char *file, char *port, char *base, char *args,
 			_exit(1);
 		}
 	case -1:
+		perror("fork");
 		break;
 	default:
 		wait(NULL);
@@ -227,6 +228,7 @@ handledcgi(int sock, char *file, char *port, char *base, char *args,
 			_exit(1);
 		}
 	case -1:
+		perror("fork");
 		break;
 	default:
 		dup2(sock, 1);

commit 7d6e2741cba15fa46a3f19c6bfcaa47f6feb558b

fix fork() regression introduced in 60e5e4b10104014295dd9c9867900f4c72a8cffe
Signed-off-by: Christoph Lohmann <20h@r-36.net>

main.c
@@ -406,6 +406,8 @@ main(int argc, char *argv[])
 		case -1:
 			perror("fork");
 			return 1;
+		case 0:
+			break;
 		default:
 			return 0;
 		}