2f292620fba218aa4d57117dadd4182799f25460
Quentin Rameau <quinq@fifth.space>
2020-04-01 20:52:22 +0200

    rc.d: simplify some rc scripts
    No need to specify default options.
    
    Signed-off-by: Christoph Lohmann <20h@r-36.net>

--- rc.d/Gentoo.conf.d
+++ rc.d/Gentoo.conf.d
@@ -1,4 +1,4 @@
 #
 # Parameters to be passed to geomyidae
 #
-GEOMYIDAE_ARGS="-d -u gopherd -g gopherd -b /var/gopher -o 70 -l /var/log/${RC_SVCNAME}.log -h localhost"
+GEOMYIDAE_ARGS="-d -u gopherd -g gopherd -l /var/log/${RC_SVCNAME}.log"
